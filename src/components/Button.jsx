9e431a9b82b8dcea54f218003ceb1190fe4df82f
Faith-Watiri <watiri216@gmail.com>
2024-01-27 22:37:51 +0300

    commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const ButtonContainer = styled.div`
-         display: flex;
-         justify-content: center:
-         margin-top: 20px;
-`;
 
 
-const StyledButton = styled.button`
-  padding: 10px 20px;
-   font-size: 16px;
-   background-color: #3498db;
-   color: #ffffff;
-   border: none;
-   border-radius: 5px;
-   cursor: pointer:
-
-   &:hover {
-    background-color: #2980b9;
-   }
-`;
-
-    
-
-
-const Button = ({ label }) => {
+    const Button = ({ label }) => {
     return (
 
         <button > { label } </button>
